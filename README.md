ddc0d0717600af427a65c787b9ac2e6862a251ff
KevinZ7 <kevin.z.zhou@gmail.com>
2020-07-06 18:58:41 -0700

    first commit, made skeleton files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Heroku app deploy and git links
+[Deply](https://murmuring-island-18099.herokuapp.com/) | [Git](https://git.heroku.com/murmuring-island-18099.git)
+
+
+#ADDTIONAL FEATURES:
+
+Character Pictures
+Sort Character by type
+Ensure tehre are no duplicate Characters
---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const { Pool } = require('pg');
+const { resourceUsage } = require('process');
+const { Console } = require('console');
+
+var pool = new Pool({
+  // connectionString: 'postgres://postgres:password@localhost/ass2'
+  connectionString: process.env.DATABASE_URL
+});
+var app = express();
+app.use(express.json());
+// app.use(express.bodyParser());
+app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => res.render('pages/main'))
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
