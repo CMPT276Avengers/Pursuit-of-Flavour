c25249507120ba628ff56090d7eb8c5e26c7fdc5
abhopla <bhoplaanubhav@gmail.com>
2020-07-07 21:07:01 -0700

    First iteration of registration form

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const PORT = process.env.PORT || 5000;
 const { Pool } = require('pg');
 const { resourceUsage } = require('process');
 const { Console } = require('console');
+var async = require('async');
 
 var pool = new Pool({
-  // connectionString: 'postgres://postgres:password@localhost/ass2'
-  connectionString: process.env.DATABASE_URL
+  connectionString: 'postgres://postgres:root@localhost/cmpt276project'
+  // connectionString: process.env.DATABASE_URL
 });
+
+// var connection =  pool.createConnection( { multipleStatements: true } );
+
 var app = express();
 app.use(express.json());
 // app.use(express.bodyParser());
@@ -19,6 +23,50 @@ app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => res.render('pages/main'))
+
+
+app.get('/register', (req, res) => res.render('pages/register'))
+
+function account_update(type, password, username){
+  var addquery = `INSERT INTO account(type,password,username) VALUES ($6,$7,$3);`; 
+
+}
+
+app.post('/adduser', (req,res)=>{
+  var fname = req.body.fname;
+  var lname = req.body.lname;
+  var username = req.body.username;
+  var email = req.body.email;
+  var phone = req.body.phone;
+  var user_type = req.body.user_type;
+  var password = req.body.password;
+  // parameter for the 'person' database
+  person_parameters = [fname,lname,username,email, phone]
+  //parameter for the 'account' database 
+  add_parameters = [user_type, password, username]
+
+  var personquery = `INSERT INTO person(fname,lname,username,email,phone) VALUES ($1,$2,$3,$4,$5);`;
+  var addquery = `INSERT INTO account(type,password,username) VALUES ($1,$2,$3);`; 
+  
+
+  pool.query(personquery, person_parameters,(error, resp)=>{
+      if (error){ return res.send(error);}
+
+      // if the information is successfully added to the person database
+      // add it other info to the account database
+      pool.query(addquery, add_parameters,(error, resp)=>{
+        if (error){ return res.send(error);}
+
+        res.redirect('/add_succ.html');
+
+      });
+
+  });
+
+  
+
+
+})
 
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
