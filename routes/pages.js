5dcd9b9a699a64827d8ea889cea11b2be22d4442
abhopla <bhoplaanubhav@gmail.com>
2020-07-09 16:36:18 -0700

    Pushing up first iteration of the my recipe page

--- routes/pages.js
+++ routes/pages.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const { Pool } = require('pg');
 const router = express.Router();
+
+var pool = new Pool({
+    connectionString: 'postgres://postgres:password@localhost/cmpt276project'
+    // connectionString: process.env.DATABASE_URL
+});
 
 //route made for page redirection
 router.get('/', (req, res) => {
@@ -40,7 +46,7 @@ router.get('/register', (req,res) => {
 
 //used for generating userview, based on user type
 router.get('/userview', (req,res) => {
-    // console.log(req.session.user);
+    // console.log(req.session.user.username);
     if(req.session.user && req.session.user.usertype == "general"){
         res.render('pages/userview',{username: req.session.user.username});
     }
@@ -51,6 +57,50 @@ router.get('/userview', (req,res) => {
         res.redirect('/login');
     }
 })
+
+router.get('/my_recipe', (req,res)=>{
+    var i = 0;
+    var rec_id = []
+    if(req.session.user){
+        var username = req.session.user.username;
+        var user = [username]
+        var getUserRecipeQuery = `SELECT recipe_id FROM exists_in WHERE username = $1`;
+        pool.query(getUserRecipeQuery,user,(error,resp)=>{
+            if (error){ return res.send(error);}
+            // var results = {'rows':result.rows}
+            if(resp.rows.length == 0){
+                res.render('pages/add_new_recipe');
+            }else{
+
+                for(i = 0; i < resp.rows.length; i++){
+                    rec_id[i] = resp.rows[i].recipe_id
+
+                }
+
+                var getRecipe = `SELECT * FROM recipes WHERE recipe_id in (${rec_id});`
+
+                pool.query(getRecipe, (error,resp)=>{
+                    if (error){ return res.send(error);}
+
+                    res.render('pages/my_recipe', resp);
+
+                })
+       
+            }
+
+
+
+        })
+    }
+    else{
+        res.render('pages/login')
+    }
+
+
+
+})
+
+
 
 
 
