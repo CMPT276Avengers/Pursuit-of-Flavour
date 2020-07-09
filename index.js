4a48a93a44287bb95ed0cf4f2152317dd2c978b0
KevinZ7 <kevin.z.zhou@gmail.com>
2020-07-09 14:58:39 -0700

    final commit before switching branch

--- index.js
+++ index.js
@@ -39,44 +39,4 @@ app.use('/auth',require('./routes/auth'));
 
 
 
-// app.get('/register', (req, res) => res.render('pages/register'))
-
-// app.post('/adduser', (req,res)=>{
-//   var fname = req.body.fname;
-//   var lname = req.body.lname;
-//   var username = req.body.username;
-//   var email = req.body.email;
-//   var phone = req.body.phone;
-//   var user_type = req.body.user_type;
-//   var password = req.body.password;
-//   // parameter for the 'person' database
-//   person_parameters = [fname,lname,username,email, phone]
-//   //parameter for the 'account' database 
-//   add_parameters = [user_type, password, username]
-
-//   var personquery = `INSERT INTO person(fname,lname,username,email,phone) VALUES ($1,$2,$3,$4,$5);`;
-//   var addquery = `INSERT INTO account(type,password,username) VALUES ($1,$2,$3);`; 
-  
-
-//   pool.query(personquery, person_parameters,(error, resp)=>{
-//       if (error){ return res.send(error);}
-
-//       // if the information is successfully added to the person database
-//       // add it other info to the account database
-//       pool.query(addquery, add_parameters,(error, resp)=>{
-//         if (error){ return res.send(error);}
-
-//         res.redirect('/add_succ.html');
-
-//       });
-
-//   });
-
-  
-
-
-// })
-
-
-
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
