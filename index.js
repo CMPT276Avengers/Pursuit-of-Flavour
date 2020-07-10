713de9ebae0ed0968ffd57f722360b9bb0a4890d
arpitgrewal12 <56005788+arpitgrewal12@users.noreply.github.com>
2020-07-09 23:07:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,128 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const { Pool } = require('pg');
+const { resourceUsage } = require('process');
+const { Console } = require('console');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
-var app =express();
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'ejs');
 
-const { Pool }=require('pg');
-var pool;
-pool=new Pool({
+var pool = new Pool({
   connectionString:'postgres://postgres:9789@localhost/cmpt276project'
 })
+// var async = require('async');
 
-  app.get('/', (req, res) => res.render('pages/index'));
+
+// var connection =  pool.createConnection( { multipleStatements: true } );
+
+var app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'scripts')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: false,
+  resave: false
+}))
 
 
+//Defining path for page directing routes: when user requests /blah it will look into the routes pages file
+app.use('/',require('./routes/pages'));
+
+//Defining path for authentication routes: when user requestls /auth/blah it will look into the routes auth file
+app.use('/auth',require('./routes/auth'));
+//Definining path for add routes /add/blah
+
+//app.use('/add',require('./routes/add'));
+
+
+  app.post('/updateuser',(req,res)=>{
+     console.log("post request for /updateuser");
+     var fname=req.body.fname;
+     var lname= req.body.lname;
+     var username=req.body.username;
+     var email=req.body.email;
+     var phone=req.body.phone;
+     var type=req.body.type;
+     var password=req.body.password;
+
+
+     if(fname != "" && fname !== undefined)
+     {
+       var update = `UPDATE person SET fname='${fname}' WHERE username='${username}'`;
+       pool.query(update,(error,results) =>{
+         if(error){
+           res.end(error);
+           console.log("UPDATE ERROR for first name")
+         }
+       }
+       )
+     }
+     if(lname != "" && lname !== undefined)
+     {
+       var update = `UPDATE person SET lname='${lname}' WHERE username='${username}'`;
+       pool.query(update,(error,results) =>{
+         if(error){
+           res.end(error);
+           console.log("UPDATE ERROR for last name")
+         }
+       }
+       )
+     }
+
+     if(email != "" && email !== undefined)
+     {
+       var update = `UPDATE person SET email='${email}' WHERE username='${username}'`;
+       pool.query(update,(error,results) =>{
+         if(error){
+           res.end(error);
+           console.log("UPDATE ERROR for email")
+         }
+       }
+       )
+     }
+     if(phone != "" && phone !== undefined)
+     {
+       var update = `UPDATE person SET phone='${phone}' WHERE username='${username}'`;
+       pool.query(update,(error,results) =>{
+         if(error){
+           res.end(error);
+           console.log("UPDATE ERROR for phone")
+         }
+       }
+       )
+     }
+     if(password != "" && password !== undefined)
+     {
+       var update = `UPDATE account SET password='${password}' WHERE username='${username}'`;
+       pool.query(update,(error,results) =>{
+         if(error){
+           res.end(error);
+           console.log("UPDATE ERROR for password")
+         }
+       }
+       )
+     }
+   res.render('pages/success');
+   });
   app.get('/database',(req,res)=> {
+      var getUsersQuery=`SELECT * FROM person,account where person.username=account.username`;
+// we are trying to get rows from here
+      pool.query(getUsersQuery, (error,result)=>{
+        if(error)
+        res.end(error); // means we are ending the error and sending it as a response
+        // if there is no error:
+        var results={'rows':result.rows} //result is an object //'rows' is a parameter
+        // result.rows is an array that contains the rows in the database table
+      res.render('pages/ad',results);//  we are sending the results to the db.ejs
+    })
+  });
+  app.post('/databasee',(req,res)=> {
       var getUsersQuery=`SELECT * FROM person,account where person.username=account.username`;
 // we are trying to get rows from here
       pool.query(getUsersQuery, (error,result)=>{
@@ -35,7 +139,7 @@ pool=new Pool({
       console.log("post request for /adduser");
       var fname=req.body.fname;
       var lname= req.body.lname;
-      var username=req.body.username;
+    var username = req.session.user.username;
       var email=req.body.email;
       var phone=req.body.phone;
       var type=req.body.type;
@@ -85,16 +189,17 @@ pool=new Pool({
 
 
     app.post('/viewuser',(req,res)=> {
-    var username=req.body.username;
-    var getUserQuery=`SELECT * FROM person WHERE username='${username}'`;// we are trying to get rows from here
-    pool.query(getUserQuery, (error,result)=>{
-      if(error)
-      res.end(error);
-      var results={'rows':result.rows} //result is an object //'rows' is a parameter
-      // result.rows is an array that contains the rows in the database table
-    res.render('pages/viewuser',results);//  we are sending the results to the db.ejs
-  })
-});
+      var username=req.body.username;
+      var getUserQuery=`SELECT * FROM person WHERE username='${username}'`;// we are trying to get rows from here
+      pool.query(getUserQuery, (error,result)=>{
+        if(error)
+        res.end(error);
+        var results={'rows':result.rows} //result is an object //'rows' is a parameter
+        // result.rows is an array that contains the rows in the database table
+      res.render('pages/viewuser',results);//  we are sending the results to the db.ejs
+    })
+  });
+
 
   app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
     //It means the port which is being used.
