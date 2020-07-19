8584f2bcc6617109026a601e40d635ab26e8867f
KevinZ7 <kevin.z.zhou@gmail.com>
2020-07-19 02:34:15 -0700

    Started to dynamically rendar the details modal

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -4,25 +4,50 @@ exports.getRecipeDetails = (req,res) => {
 
         var recipeId = req.query.recipeId;
 
-        // axios({
-        //     "method":"GET",
-        //     "url":"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/"+recipeId+"/information",
-        //     "headers":{
-        //     "content-type":"application/octet-stream",
-        //     "x-rapidapi-host":"spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-        //     "x-rapidapi-key":"bffc1f9da3msh5395e6eda5e41aep1fbc6fjsn5a9e415e3423",
-        //     "useQueryString":true
-        //     }
-        // })
-        // .then((response)=>{
-        //       var data = response;
+        axios({
+            "method":"GET",
+            "url":"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/"+recipeId+"/information",
+            "headers":{
+            "content-type":"application/octet-stream",
+            "x-rapidapi-host":"spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+            "x-rapidapi-key":"bffc1f9da3msh5395e6eda5e41aep1fbc6fjsn5a9e415e3423",
+            "useQueryString":true
+            }
+        })
+        .then((response1)=>{
+              var recipeInformation = response1.data;
+              console.log(recipeInformation);
 
-        // })
-        // .catch((error)=>{
-        //       console.log(error)
-        // })
+              axios({
+                "method":"GET",
+                "url":"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/"+recipeId+"/analyzedInstructions",
+                "headers":{
+                "content-type":"application/octet-stream",
+                "x-rapidapi-host":"spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+                "x-rapidapi-key":"bffc1f9da3msh5395e6eda5e41aep1fbc6fjsn5a9e415e3423",
+                "useQueryString":true
+                }
+              })
+              .then((response2)=>{
+                  var recipeInstructions = response2.data;
+                  console.log("--------------------------------------------");
+                  console.log(recipeInstructions);
+    
+                  res.send({recipeInfo: recipeInformation,recipeInstructions: recipeInstructions});
 
-        res.send({name: "lampchops"});
+    
+               })
+               .catch((error)=>{
+                  console.log(error);
+                  res.send(404);
+                })
+
+        })
+        .catch((error)=>{
+              console.log(error);
+              res.send(404);
+        })
+
 
 
 }
--- scripts/recipeDetails.js
+++ scripts/recipeDetails.js
@@ -1,10 +1,26 @@
 $(document).ready(function(){
+    $('#recipeModal').modal({show:true});
     $('.openBtn').on('click',function(){
         var recipeId = $(this).attr("data-recipeId");
         $.get("/recipes/details?recipeId="+recipeId, function(data){
-            console.log("done");
-            $('.modal-title').append(data.name);
+            //for title
+            $('.modal-title').append(data.recipeInfo.title);
 
+            //for image
+            $('.recipe-image').append("<img src="+data.recipeInfo.image+" class = 'img-responsive rounded' width='100%'>");
+            
+            //section for general info
+            if(data.recipeInfo.cheap == true){
+                $('.general-info-details').append("<div style:'color: green;'>$</div>");
+            } else {
+                $('.general-info-details').append("<div style:'color: red;'>$$$</div>");
+            }
+            $('.general-info-details').append("<div>"+data.recipeInfo.readyInMinutes+" min</div>");
+            $('.general-info-details').append("<div>"+data.recipeInfo.servings+"</div>");
+            $('.addtional-info').append("<div>"+data.recipeInfo.servings+"</div>");
+
+            
+            
             $('#recipeModal').modal({show:true});
         })
     });
