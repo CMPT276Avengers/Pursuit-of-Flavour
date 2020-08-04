13756885234441c04d229560a1d131eaa05c17b9
TingTing Li <tingtingli0325@gmail.com>
2020-08-04 15:37:38 -0700

    Updated and fixed login/registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,13 +10,13 @@ exports.login = (req,res) => {
     const {username, password} = req.body;
 
     if(!username || !password){
-        return res.status(400).render('pages/login', {message: 'Please provide an email/password'})
+        return res.status(400).render('pages/loginerror', {message: 'Please provide an email/password'})
     }
 
     var selectUsername = 'SELECT * FROM account WHERE username = $1';
     pool.query(selectUsername,[username], async(error,results) => {
         if(results.rows.length == 0 || results.rows[0].password != password){
-            res.status(401).render('pages/login', {message: 'Email or Password is incorrect'});
+            res.status(401).render('pages/loginerror', {message: 'Email or Password is incorrect'});
         }
         else{
             // console.log(results.rows[0]);
@@ -59,18 +59,23 @@ exports.adduser = (req,res) => {
     var personquery = `INSERT INTO person(fname,lname,username,email,phone) VALUES ($1,$2,$3,$4,$5);`;
     var addquery = `INSERT INTO account(type,password,username) VALUES ($1,$2,$3);`;
 
+    if(username != '' || password != ''){
 
-    pool.query(personquery, person_parameters,(error, resp)=>{
-        if (error){ return res.status(409).send(error);}
+        pool.query(personquery, person_parameters,(error, resp)=>{
+            if (error){ return res.status(409).send(error);}
 
-        // if the information is successfully added to the person database
-        // add it other info to the account database
-        pool.query(addquery, add_parameters,(error, resp)=>{
-          if (error){ return res.status(409).send(error);}
+            // if the information is successfully added to the person database
+            // add it other info to the account database
+            pool.query(addquery, add_parameters,(error, resp)=>{
+            if (error){ return res.status(409).send(error);}
 
-          res.status(201).redirect('/login');
+            res.status(201).redirect('/login');
+
+            });
 
         });
-
-    });
+    }
+    else{
+        res.status(201).redirect('/login');
+    }
 }
