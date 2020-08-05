36f2287b43c51f0b58d2c40ea5f0b93e55ce6ea1
KevinZ7 <kevin.z.zhou@gmail.com>
2020-08-04 21:32:33 -0700

    finished comments

--- controllers/comment.js
+++ controllers/comment.js
@@ -5,3 +5,24 @@ var pool = new Pool({
 //    connectionString: process.env.DATABASE_URL
 });
 const session = require('express-session');
+
+exports.getRecipeComments = (req,res) => {
+    var recipeId = req.query.recipeId;
+    var query = 'SELECT * from comment where recipe_id = $1'
+    pool.query(query,[recipeId],(error,results) => {
+        res.send(results.rows);
+    })
+}  
+
+exports.postComment = (req,res) => {
+    var username = req.session.user.username;
+    var recipeId = req.body.data.recipeId;
+    var context = req.body.data.context;
+    var time = new Date();
+
+    var query = "INSERT INTO comment (username,recipe_id,context,time) VALUES ($1,$2,$3,$4);"
+    pool.query(query,[username,recipeId,context,time],function(error,results){
+        res.sendStatus(202);
+    })
+
+}
--- routes/comment.js
+++ routes/comment.js
@@ -2,7 +2,7 @@ const express = require('express');
 const commentController = require('../controllers/comment');
 const router = express.Router();
 
-
-
+router.get('/getRecipeComments', commentController.getRecipeComments);
+router.post('/postComment', commentController.postComment);
 
 module.exports = router;
